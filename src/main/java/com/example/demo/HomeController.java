ecf1a8195da3e7ec96ca4bc99de09d84b9c0016a
Mateusz <mateusz.tobiasz@grupakrawczyk.pl>
2022-04-07 23:31:44 +0200

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World!";
+    }
+
+
+    @RequestMapping("/example")
+    public String custom() {
+        return "custom";
+    }
+
+
+//    @RequestMapping("/test/{name}-{age}")
+//    @ResponseBody
+//
+//    public String index() {
+//        @privateVariable String name,
+//        return "Hello World!";
+//    }
+
+
+}
