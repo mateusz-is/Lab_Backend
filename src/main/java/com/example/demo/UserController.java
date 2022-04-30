1c0faf8a6899f52fb63da3920a3abdd885490316
Mateusz <mateusz.tobiasz@grupakrawczyk.pl>
2022-04-30 09:34:03 +0200

    Ostatnie zadania

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,21 +16,23 @@ import java.util.Map;
 public class UserController {
     private final Map<Integer, UserEntity> users;
     private final ObjectMapper objectMapper;
+    @Autowired
+    private UserService userService;
 
     public UserController()
     {
         users = new HashMap<>();
-        users.put(0,new UserEntity("Franciszek", 20));
-        users.put(1,new UserEntity("Ewa", 23));
-        users.put(1,new UserEntity("Adam", 18));
+        users.put(0,new UserEntity("Adam", 22, 0));
+        users.put(1,new UserEntity("Ewa", 23,1));
+        users.put(1,new UserEntity("Józef", 40,1));
         objectMapper = new ObjectMapper();
     }
-
     @RequestMapping("/users")
     @ResponseBody
     public String users() throws JsonProcessingException {
         return objectMapper.writeValueAsString(users);
     }
+
 
     @RequestMapping("/users/{id}/get")
     @ResponseBody
@@ -40,13 +42,55 @@ public class UserController {
     @RequestMapping("/users/{id}/remove")
     public String deleteUser (@PathVariable int id) {
         users.remove(id);
-        return ("User: "+id+" removed!");
+        return ("User "+id+" removed!");
     }
     @RequestMapping("/users/add")
-    @ResponseBody
-    public String addUser (@RequestParam(name="name") String name, @RequestParam(name="age") int age) throws JsonProcessingException {
+    public String addUser (@RequestParam(name="name") String name, @RequestParam(name="age") int age) {
         users.put(users.size(), new UserEntity(name,age));
-        UserEntity user = users.get(users.size()-1);
-        return objectMapper.writeValueAsString(user);
+        return String.format("User %s with age %d created", name, age);
+    }
+
+    @RequestMapping("/api/users")
+    @ResponseBody
+    public UsersPage apiUsers (@RequestParam(name="page-number", defaultValue="1") Integer pageNumber,
+                               @RequestParam(name="page-size", defaultValue = "20") Integer pageSize)
+    {
+        return userService.getUsers(pageNumber, pageSize, users);
+    }
+
+    @RequestMapping(
+            value = "/api/user/create",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public UserEntity createUser(@RequestBody UserEntity user) {
+
+        return userService.createUser(users, user);
+    }
+
+    @RequestMapping("/api/users/{id}")
+    @ResponseBody
+    public UserEntity getUserById (@PathVariable int id) {
+        return userService.getUser(users,id);
+    }
+
+    @RequestMapping(
+            value = "/api/users/{id}/update",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public UserEntity updateUser(@RequestBody UserEntity user, @PathVariable int id) {
+
+        return userService.updateUser(users, user, id);
+    }
+
+    @RequestMapping("/api/users/{id}/remove")
+    @ResponseBody
+    public HttpStatus removeUserById (@PathVariable int id) {
+        return userService.removeUser(users,id);
     }
 }
--- src/main/java/com/example/demo/UserEntity.java
+++ src/main/java/com/example/demo/UserEntity.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
 public class UserEntity {
+    public int Id;
     public String Name;
     public int Age;
+
 
     public UserEntity(String name, int age) {
         Name=name;
         Age=age;
+    }
+    public UserEntity(String name, int age,int id) {
+        Id = id;
+        Name = name;
+        Age = age;
     }
 }
