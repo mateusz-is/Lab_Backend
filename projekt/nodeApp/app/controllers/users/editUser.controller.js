0c8cb7ef29320968c2eeb96507b1bc51a176258e
Mateusz <mateusz.tobiasz32@gmail.com>
2022-07-06 22:32:29 +0000

    Walidacja adresu email. Sprawdzanie czy taki sam adres email istnieje w bazie

--- projekt/nodeApp/app/controllers/users/editUser.controller.js
+++ projekt/nodeApp/app/controllers/users/editUser.controller.js
@@ -6,10 +6,12 @@ const user = db.user;
 exports.updateUser = async (req, res) => {
     const id = req.body.id;
 
-    if (!req.body.email) {
-        await res.status(400).send({ message: "Content can not be empty!" });
-        return;
-    }
+    const permission = req.user?.tokenObject?.permission
+    const currentUserId = req.user?.tokenObject?.id.toString()
+
+    let getUser = await user.findById(id),
+        editUserId = getUser?._id.toString()
+
     let userData = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -19,15 +21,35 @@ exports.updateUser = async (req, res) => {
         isActive: req.body.isActive
     }
 
-    let emailExist = await user.findOne({ email: req.body.email });
+    const editUser = async () => {
+        let emailExist = await user.findOne({ email: req.body.email });
+        if (!req.body.email) {
+            await res.status(400).send({ message: "Content can not be empty!" });
+            return;
+        }
+        const salt = await bcrypt.genSalt(10);
+        userData.password = await bcrypt.hash(userData.password.toString(), salt);
 
-    if (emailExist) return res.status(400).send('That user already exisits!');
+        const updateUser = async () =>{
+            await user.findByIdAndUpdate(id, userData)
+            await res.status(200).send(userData);
+        }
 
-    const salt = await bcrypt.genSalt(10);
-    userData.password = await bcrypt.hash(userData.password.toString(), salt);
+        if (emailExist?.email !== undefined) {
+            if (emailExist?.email !== getUser?.email) return res.status(400).send('That user already exisits!');
+            await updateUser()
+        } else {
+            await updateUser()
+        }
+    }
 
-    await user.findByIdAndUpdate(id, userData)
-    await res.status(200).send(userData);
+    if (permission === 'admin') {
+        return await editUser();
+    } else if (editUserId === currentUserId) {
+        return await editUser()
+    } else {
+        return await res.status(401).send("You need Admin permission to edit this user")
+    }
 
 }
 
