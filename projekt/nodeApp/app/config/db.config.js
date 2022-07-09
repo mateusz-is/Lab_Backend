890b5ec4d5530b7e502195fc2c699bc602193c33
Mateusz <mateusz.tobiasz32@gmail.com>
2022-07-09 12:16:53 +0000

    Popoprawa zmiannych do połączenia z bazą

--- projekt/nodeApp/app/config/db.config.js
+++ projekt/nodeApp/app/config/db.config.js
@@ -1,4 +1,5 @@
-module.exports = {
-  url: 'mongodb+srv://mtobiasz:bHnq2GGWK7F4pYM9@cluster0.ixzne.mongodb.net/db_wsei?retryWrites=true&w=majority'
-};
+require('dotenv').config();
 
+module.exports = {
+  url: process.env.MAINAPP
+}
